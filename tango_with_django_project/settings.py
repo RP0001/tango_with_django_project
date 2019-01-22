b67a0c64d9086ef54f059309ee4c852cee49e89b
Radu Petrescu <2411100P@student.gla.ac.uk>
2019-01-22 23:03:23 +0000

    commit_2_chapter_3

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rango', #rango is not recognised as an app
 ]
 
 MIDDLEWARE = [
