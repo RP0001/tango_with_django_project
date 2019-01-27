cd4ad20842846c033015f8fb56d90465d5537406
Radu Petrescu <2411100P@student.gla.ac.uk>
2019-01-27 01:25:58 +0000

    commit_7_chapter_5

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+# Category inherits from Model
+# Each of these classes are really tables in the database
+# python manage.py sqlmigrate rango 0001 to view underlying sql
+# migration is a fancy term for sql_create
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+class Page(models.Model):
+
+    category = models.ForeignKey(Category)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+    # This is toString()
+    def __str__(self):
+        return self.title
+
