76851fd78d1b0f3bbaa5aa9717c81a5e794bc5fa
Radu Petrescu <2411100P@student.gla.ac.uk>
2019-02-07 01:04:04 +0000

    commit_9_chapter_7

--- rango/urls.py
+++ rango/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^about/', views.about, name='about'), #add url pattern for about page
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.show_category, name='show_category'),
     url(r'^page/(?P<page_name_slug>[\w\-]+)/$', views.show_page, name='show_page'),
+    url(r'^add_category/$', views.add_category, name='add_category'),
 
 ]
