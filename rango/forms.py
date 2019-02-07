8e93ccf5af1c98569fe8e584ba868eb8598160c6
Radu Petrescu <2411100P@student.gla.ac.uk>
2019-02-07 17:30:31 +0000

    commit_15_chapter_7

--- rango/forms.py
+++ rango/forms.py
@@ -5,7 +5,7 @@ from rango.models import name_max_length
 
 
 class CategoryForm(forms.ModelForm):
-    name = forms.CharField(max_length=name_max_length, help_text="Please enter the category name. Field required:", required=True)
+    name = forms.CharField(max_length=name_max_length, help_text="Please enter the category name.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
